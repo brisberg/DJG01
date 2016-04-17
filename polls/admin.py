2f27c49b46aa48ea889d9ee328c338ba790fa1fc
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-16 22:10:54 -0700

    Added css stylesheet and background image

--- polls/admin.py
+++ polls/admin.py
@@ -4,4 +4,11 @@ from .models import Question
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
