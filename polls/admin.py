baf46adf6d322829f3391d6576812e87245e07fd
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-16 22:42:09 -0700

    Updated the building Django admin pages. Part 7 done

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -10,5 +15,9 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
