63d65c7652260557fee8b388f7607ff9457eeb3f
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-16 21:45:06 -0700

    Testing for detail view

--- polls/tests.py
+++ polls/tests.py
@@ -81,7 +81,6 @@ class QuestionViewTests(TestCase):
 
 
 class QuestionMethodTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() should return False for questions whose pub_date is in the future.
@@ -100,7 +99,7 @@ class QuestionMethodTests(TestCase):
         old_question = Question(pub_date=time)
         self.assertEqual(old_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_old_question(self):
+    def test_was_published_recently_with_recent_question(self):
         """
         was_published_recently() should return True for questions whose pub_date is within the last day.
         :return:
@@ -108,3 +107,28 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.',
+                                          days=5)
+        response = self.client.get(reverse('polls:detail',
+                                           args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                           args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
